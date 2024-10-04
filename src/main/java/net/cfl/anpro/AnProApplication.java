0ef2bac7070efffe8eead563ed431efff682982a
ludmila <lm738463@gmail>
2024-10-04 10:31:33 -0300

    commit inicial

---
+++ src/main/java/net/cfl/anpro/AnProApplication.java
@@ -0,0 +1,13 @@
+package net.cfl.anpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnProApplication.class, args);
+	}
+
+}
